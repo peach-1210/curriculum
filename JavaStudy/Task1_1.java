f41b8fc83983035f59ffbf8c863c34649241824e
peach-1210 <85363762+peach-1210@users.noreply.github.com>
2021-06-10 11:30:29 +0900

    課題提出テスト
    1₋1、Task1_1.javaの作成

---
+++ JavaStudy/Task1_1.java
@@ -0,0 +1,28 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        int hogeInt = 50;
+
+        String hogeString = "テスト";
+
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [double型のhogeDoubleという変数を扱う。初期値は0.2です。]
+        
+        double hogeDouble = 0.2;
+
+         hogeInt = 100;
+
+          hogeString = "合格";
+
+         System.out.println(hogeInt);
+
+          // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // [hogeStringを表示させるよう指示している。]
+        System.out.println(hogeString);
+
+        System.out.println(0.2);
+
+
+    }
+}
