be7c306dc44bad48d3437458a4621d068481fca4
peach-1210 <85363762+peach-1210@users.noreply.github.com>
2021-11-20 17:32:40 +0900

    問題の提出
    GetPrimeNumberの提出

---
+++ JavaStudy/GetPrimeNumber/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	
+	public static int [] prime = {3,7,45,1,6,75,85,24,11,87};
+		public static void main(String []args) {
+		for(int i=0; i < prime.length;i++) {
+			System.out.println(prime[i]);
+			
+		}
+	}
+}
